ea4186b513c17e819696ba9ae5327801594031bf
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-04-27 00:30:57 +0300

    fix: edit rate limit

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const { DB_URL, PORT } = require('./config');
 const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
-const limiter = require('./middlewares/rate-limiter');
+const { limiter } = require('./middlewares/rate-limiter');
 
 const app = express();
 app.use(cors);
-
 app.use(limiter);
 
 app.use(express.json());
--- middlewares/rate-limiter.js
+++ middlewares/rate-limiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
-module.exports = () => {
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // за 15 минут
-    max: 20, // можно совершить максимум 100 запросов с одного IP
-  });
-};
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // за 15 минут
+  max: 2, // можно совершить максимум 100 запросов с одного IP
+});
+
+module.exports = { limiter };

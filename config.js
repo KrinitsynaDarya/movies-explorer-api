7656c5dc1d24112267ef63de04fbb41477eaa32d
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-04-26 23:32:53 +0300

    fix: fix eslint problems

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET ?? 'dev-token',
-  DB_URL: process.env.DB_URL ?? 'mongodb://127.0.0.1:27017/bitfilmsdb',
-  PORT: process.env.PORT ?? 3000,
-}
+  JWT_SECRET: process.env.JWT_SECRET !== null ? 'dev-token' : process.env.JWT_SECRET,
+  DB_URL: process.env.DB_URL !== null ? 'mongodb://127.0.0.1:27017/bitfilmsdb' : process.env.DB_URL,
+  PORT: process.env.PORT !== null ? 3000 : process.env.PORT,
+};
